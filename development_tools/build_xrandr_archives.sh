0b8cb51a29faabc0d9dd8f1192e486e1fa41ac33
chrysn <chrysn@fsfe.org>
2015-01-10 10:56:35 +0100

    allow different output files

--- development_tools/build_xrandr_archives.sh
+++ development_tools/build_xrandr_archives.sh
@@ -2,8 +2,11 @@
 
 set -e
 
+OUTFILENAME="$1"
+shift
+
 for t in "$@"
 do
 	echo --$t--
-	PATH=$t:$PATH python3 -m screenlayout.executions.run --zip-out $t/output.zip --zip-out-stateless --shell --verbose --ignore-errors "xrandr --query --verbose" "xrandr --help" "xrandr --version"
+	PATH=$t:$PATH python3 -m screenlayout.executions.run --zip-out $t/$OUTFILENAME --zip-out-stateless --shell --verbose --ignore-errors "xrandr --query --verbose" "xrandr --help" "xrandr --version"
 done
